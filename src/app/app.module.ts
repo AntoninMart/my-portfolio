7ffae528b8d966aed6851b0d9ec9d2bab39bf8e0
Antonin Martinez <antonin.martinez@inetum.com>
2023-10-26 17:25:17 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AccueilComponent } from './components/accueil/accueil.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    AccueilComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
