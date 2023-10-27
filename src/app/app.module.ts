a6640ffba9f6a3dcb37b619a4db801771bf4e211
Antonin Martinez <antonin.martinez@inetum.com>
2023-10-27 15:19:37 +0200

    ajout de mes formations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { AboutComponent } from './components/about/about.component';
 import { HardskillsComponent } from './components/hardskills/hardskills.component';
 import { ProjectComponent } from './components/project/project.component';
 import { HobbiesComponent } from './components/hobbies/hobbies.component';
+import { FormationComponent } from './components/formation/formation.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { HobbiesComponent } from './components/hobbies/hobbies.component';
     AboutComponent,
     HardskillsComponent,
     ProjectComponent,
-    HobbiesComponent
+    HobbiesComponent,
+    FormationComponent
   ],
   imports: [
     BrowserModule,
