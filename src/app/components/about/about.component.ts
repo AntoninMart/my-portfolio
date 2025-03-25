0f5c1f85c8d7f1c8e869ed3255ed1664655d346b
Antonin Martinez <antonin.martinez>
2025-03-25 09:52:04 +0100

    mise à jour info

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent {
+  public age: number = 0;
 
+  constructor() { }
+
+  ngOnInit(): void {
+    this.calculateAge();
+  }
+
+  calculateAge(): void {
+    const birthDate = new Date('2002-10-16');
+    let timeDiff = Math.abs(Date.now() - birthDate.getTime());
+    this.age = Math.floor((timeDiff / (1000 * 3600 * 24))/365.25);
+  }
 }
--- src/app/components/list-projets/list-projets.component.ts
+++ src/app/components/list-projets/list-projets.component.ts
@@ -9,11 +9,31 @@ export class ListProjetsComponent {
   constructor() { }
   public projets = [
     {
+      titre : "ANPS",
+      icone : "logo-anps.svg",
+      description : "Convertion du site web wordpress de l'association nationnal des podologues du sport en une application web fullstack. Réalisation de du projet avec le framework angular et symfony.",
+      link : "https://anps-france.com/",
+      date : "Fév. 2025",
+    },
+    {
+      titre : "CLEVA",
+      icone : "logo-cleva.png",
+      description : "Conceptualisation et développement d'une solution web complète, comprenant une interface utilisateur et administrateur. L'application permet de gérer le parking de l'entreprise. Gestion des demande de stationnement, d'attribution de place automatique et manuel etc... Réalisation du projet avec le framework angular et Java springboot.",
+      date : "Mai. 2024 - Juil. 2024",
+    },
+    {
+      titre : "A fleur de peau",
+      icone : "a-fleur-de-peau.svg",
+      description : "Création d'une boutique e-commerce pour un salon de massage. Réalisation de du projet avec wordpress et le plugin woocommerce.",
+      link : "https://afleurdepeau.eu/",
+      date : "Fév. 2025",
+    },
+    {
       titre : "Noneo",
       icone : "noneo.svg",
       description : "Associé fondateur de Noneo. Noneo est une société de prestations numérique, nous nous chargons d'offrir à nos clients une présence sur internet en leurs créant des sites internet ou solution numérique adapté à tout leurs besoin",
       link : "https://noneo.fr",
-      date : "Oct. 2023 - Maintenant",
+      date : "Oct. 2023 - Juin 2024",
     },
     {
       titre : "Apprendre les sons",
